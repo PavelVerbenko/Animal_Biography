9fcbf4468c7253d2eee129cfb4fa88ddaf41a121
Pavel Verbenko <ya.pasha.verbenko@yandex.ru>
2024-07-30 18:16:48 +0300

    Тест

--- src/main/java/UsingMenuApplication/UsingMenuDemo.java
+++ src/main/java/UsingMenuApplication/UsingMenuDemo.java
@@ -30,7 +30,7 @@ class MyFrame extends JFrame implements ActionListener {
 
     class MyButton extends JButton {
         MyButton(String txt) {
-            super(new ImageIcon("C:\\Users\\dinky\\Downloads\\проект JavaCore\\buttons\\" + txt));
+            super(new ImageIcon("C:\\Users\\dinky\\OneDrive\\Рабочий стол\\Java\\Java Core\\Уровень 9 Меню и панель инструментов\\Menu and toolbar\\src\\main\\java\\UsingMenuApplication\\img\\buttons\\" + txt));
             setFocusPainted(false);
         }
     }
@@ -75,16 +75,110 @@ class MyFrame extends JFrame implements ActionListener {
         animals = new JMenuItem[cyrNames.length];
 
         for (int k = 0; k < engNames.length; k++) {
-            imgs[k] = new ImageIcon("C:\\Users\\dinky\\Downloads\\проект JavaCore\\animal\\" + engNames[k] + "jpg");
-            files[k] = "C:\\Users\\dinky\\Downloads\\проект JavaCore\\animal\\" + engNames[k] + ".txt";  //разобрать тут
+            imgs[k] = new ImageIcon("C:\\Users\\dinky\\OneDrive\\Рабочий стол\\Java\\Java Core\\Уровень 9 Меню и панель инструментов\\Menu and toolbar\\src\\main\\java\\UsingMenuApplication\\img\\animal\\" + engNames[k] + "jpg");
+            files[k] = "C:C:\\Users\\dinky\\OneDrive\\Рабочий стол\\Java\\Java Core\\Уровень 9 Меню и панель инструментов\\Menu and toolbar\\src\\main\\java\\UsingMenuApplication\\img\\animal\\" + engNames[k] + ".txt";  //разобрать тут
             Image img = imgs[k].getImage();
             Image scaledImg = img.getScaledInstance(150, 100, Image.SCALE_DEFAULT);
             imgs[k] = new ImageIcon(scaledImg);
         }
 
         tb = new JToolBar("Панель меню");
-        exitBtn = new MyButton("exit.png");
+        exitBin = new MyButton("exit.png");
         exitBin.setToolTipText("Завершение работы");
+        startBtn = new MyButton("start.png");
+        startBtn.setToolTipText("Начальное изображение");
+        prevBtn = new MyButton("prev.png");
+        prevBtn.setToolTipText("Предыдущее изображение");
+        nextBtn = new MyButton("next.png");
+        nextBtn.setToolTipText("Следующее изображение");
+        tb.add(exitBin);
+        tb.add(startBtn);
+        tb.add(prevBtn);
+        tb.add(nextBtn);
+
+        add(tb, BorderLayout.NORTH);
+
+        pnl = new JPanel();
+        pnl.setBorder(bdr);
+        pnl.setLayout(new GridLayout(1, 2));
+        lbl = new JLabel();
+        lbl.setHorizontalAlignment(JLabel.CENTER);
+        pnl.add(lbl);
+        add(pnl, BorderLayout.CENTER);
+        JScrollPane sp = new JScrollPane();
+        tp = new JTextPane();
+        tp.setEditable(false);
+        sp.getViewport().add(tp);
+        pnl.add(sp);
+        btn = new JButton("OK");
+        btn.setFocusPainted(false);
+        JPanel p = new JPanel();
+        p.setLayout(new GridLayout(1, 3));
+        p.setBorder(bdr);
+        p.add(new JPanel());
+        p.add(btn);
+        p.add(new JPanel());
+        add(p, BorderLayout.SOUTH);
+
+        mb = new JMenuBar();
+        content = new JMenu();
+        content = new JMenu("Содержимое");
+        view = new JMenu("Вид");
+        program = new JMenu("Программа");
+
+        about = new JMenuItem("О программе");
+        exit = new JMenuItem("Выход", exitBin.getIcon());
+        program.add(about);
+        program.addSeparator();
+        program.add(exit);
+
+        color = new JCheckBoxMenuItem("Цвет панели", true);
+
+        Light = new JRadioButtonMenuItem("Светлый", false);
+        Dark = new JRadioButtonMenuItem("Черный", false);
+        Ordinary = new JRadioButtonMenuItem("Обычный", true);
+        Pink = new JRadioButtonMenuItem("Розовый", false);
+        Blue = new JRadioButtonMenuItem("Синий", false);
+        Green = new JRadioButtonMenuItem("Зеленый", false);
+        Cyan = new JRadioButtonMenuItem("Морской", false);
+        Red = new JRadioButtonMenuItem("Красный", false);
+        Yellow = new JRadioButtonMenuItem("Желтый", false);
+        Orange = new JRadioButtonMenuItem("Оранжевый", false);
+        Gray = new JRadioButtonMenuItem("Серый", false);
+
+        ButtonGroup bg = new ButtonGroup();
+        bg.add(Light);
+        bg.add(Dark);
+        bg.add(Ordinary);
+        bg.add(Pink);
+        bg.add(Blue);
+        bg.add(Green);
+        bg.add(Cyan);
+        bg.add(Red);
+        bg.add(Yellow);
+        bg.add(Orange);
+        bg.add(Gray);
+
+        view.add(color);
+        view.addSeparator();
+        view.add(Light);
+        view.add(Dark);
+        view.add(Ordinary);
+        view.add(Pink);
+        view.add(Blue);
+        view.add(Green);
+        view.add(Cyan);
+        view.add(Red);
+        view.add(Yellow);
+        view.add(Orange);
+        view.add(Gray);
+
+        for (int k = 0; k < animals.length; k++) {
+            animals[k] = new JMenuItem(cyrNames[k]);
+            animals[k].setActionCommand("" + k);
+            animals[k].addActionListener(this);
+            content.add(animals[k]);
+        }
     }
 }
 
